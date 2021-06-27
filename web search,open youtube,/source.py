ce1e38047effc7eeedd000b41bd23b5ef3b29b54
Tpras123 <63368873+Tpras123@users.noreply.github.com>
2021-06-27 00:43:40 -0700

    Create source.py

---
+++ web search,open youtube,/source.py
@@ -0,0 +1,7 @@
+  
+  
+import pywhatkit
+
+pywhatkit.sendwhatmsg("+91xxxxxxxxx","message",12,16)
+pywhatkit.search("cloud computing")#for opening google  webpages
+pywhatkit.playonyt("https://youtu.be/SB1PTrQrMJM")#for opening youtube
